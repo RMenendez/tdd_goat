8e3e069921a471d766ee26bc9a21da6a6dd0412f
RMenendez <rmgiacomini@gmail.com>
2024-11-23 13:49:13 +0100

    End of chapter seven. All tests ok

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # tdd_goat
 
+# To run the django server
+python manage.py runserver
+
+# To run unit tests
+python manage.py test lists
+
+# To run the django server
+python manage.py test functional_tests
+
+
 https://www.obeythetestinggoat.com/book/chapter_02_unittest.html
---
+++ lists/urls.py
@@ -0,0 +1,12 @@
+"""lists URL Configuration
+
+It is included in superlists/urls.py. 
+"""
+from django.urls import path
+from lists import views
+
+urlpatterns = [
+    path('new', views.new_list, name='new_list'),
+    path('<int:list_id>/', views.view_list, name='view_list'),
+    path('<int:list_id>/add_item', views.add_item, name='add_item'),
+]
