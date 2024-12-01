3a2ae5ef1df3042a092be5293fc50ab1c26bce00
RMenendez <rmgiacomini@gmail.com>
2024-12-02 00:11:59 +0100

    At the end of chapter 10 - all test OK

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -130,3 +130,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "loggers": {
+        "root": {"handlers": ["console"], "level": "INFO"},
+    },
+}
